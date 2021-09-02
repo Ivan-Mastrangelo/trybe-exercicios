f90a3525405b9db7b8cbbef66ae7b0f8d5a60750
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-02 04:59:18 -0300

    add treino em for/in

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticandoForIn.js
@@ -0,0 +1,16 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+/**let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let banana in car) {
+    console.log(banana, car[banana]);
+  }
+**/
