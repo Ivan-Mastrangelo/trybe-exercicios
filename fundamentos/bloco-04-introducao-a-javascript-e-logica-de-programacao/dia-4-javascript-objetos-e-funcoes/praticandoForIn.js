3c421fae8ce3c2ddda3b6760d5189200d6b7b9dd
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-02 12:13:53 -0300

    add pratica for/in feita

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticandoForIn.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticandoForIn.js
@@ -1,9 +1,8 @@
-let cars = ['Saab', 'Volvo', 'BMW'];
+//let cars = ['Saab', 'Volvo', 'BMW'];
 
-for (let index in cars) {
-  console.log(cars[index]);
-}
-
+//for (let index in cars) {
+ // console.log(cars[index]);
+//}
 /**let car = {
     type: 'Fiat',
     model: '500',
@@ -14,3 +13,35 @@ for (let index in cars) {
     console.log(banana, car[banana]);
   }
 **/
+//1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+/**
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let key in names) {
+  console.log('Olá ' + names[key]);
+}; */
+
+//2 - Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+/**let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for (let key in car) {
+  console.log(key, car[key]);
+} */
+
+/**let car = ['audi', 'volks', 'ford'];
+for (let key in car) {
+  console.log(key, car[key]);
+}**/
+
+
+
+
+
