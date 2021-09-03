7f232ccf069e059629fa72bce9826bf878f47993
Ivan-Mastrangelo <masttrangello@gmail.com>
2021-09-03 14:59:55 -0300

    salvando o dia anterior

---
+++ fundamentos/bloco-05-javascript-DOM-eventos-e-webStorage/script.js
@@ -0,0 +1,16 @@
+const paragraph = document.getElementById("paragraph"); paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Star Wars'
+
+document.getElementById('second-paragraph').innerText = 'fazendo testes e testes para aprander a programar'
+      
+document.getElementById('subtitle').innerText = 'mais um teste'
+
+let alteracaoDoP1 = document.getElementsByClassName('textos')[0];
+alteracaoDoP1.style.fontSize = '25px';
+
+let corSubtitulo = document.getElementsByTagName('h4')[0];
+corSubtitulo.style.color = 'green';
+
+
+
